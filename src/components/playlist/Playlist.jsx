f93a53aabf9269a051ce4f3d2f6b052f88457578
augusto <augustoantonelli88@gmail.com>
2023-01-13 13:56:46 -0300

    create playlist done

--- src/components/playlist/Playlist.jsx
+++ src/components/playlist/Playlist.jsx
@@ -23,11 +23,24 @@ export default function Playlist() {
         });
     }, []);
 
+    const save = () => {
+        axios.post('http://192.168.1.37:8888/create', 
+        {
+            songs_list: uriList
+        }, 
+        { 
+            withCredentials: true 
+        })
+        .then(response => {
+            console.log(response);
+        })
+    }
+
 
     return(
         <div className="playlist">
             <MovileNavbar/>
-
+            <button className='save_button' onClick={save}> SAVE! </button>
             {
                 Data.map((song, index) => <Song key={index} song_name={song.song_name} img={song.img.url} artist_name={song.artist_name} song_uri={song.song_uri} uriList={{'uriList':uriList, 'setUriList':setUriList}}/>)
             }
