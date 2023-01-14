5940febf24b05d2483a9f2743f8f571138aee8a3
augusto <augustoantonelli88@gmail.com>
2023-01-14 16:21:40 -0300

    reguistration page done

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import TextField from '@mui/material/TextField';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
 // Style
 import './Register.css'
@@ -33,37 +35,48 @@ const CustomTextField = styled(TextField)({
 });
 
 export default function Register() {
-    const {register, handleSubmit} = useForm()
+    const [open, setOpen] = useState(false);
+    const { register, handleSubmit } = useForm()
 
-    function login(){
+    function login() {
         axios.get(process.env.REACT_APP_BACKENDURL + '/login')
-        .then((response) => {
-            // console.log("llega al login")
-            // console.log(response.data)
-            window.location.replace(response.data);
-        });
+            .then((response) => {
+                // console.log("llega al login")
+                // console.log(response.data)
+                window.location.replace(response.data);
+            });
     }
 
-    function onSubmit(data){
-        axios.post(process.env.REACT_APP_BACKENDURL + '/register', 
-        {
-            user: data.usuario,
-            email: data.email
-        }, 
-        { 
-            withCredentials: true 
-        })
-        .then(response => {
-            console.log(response);
-        })
+    function onSubmit(data) {
+        setOpen(true);
+        axios.post(process.env.REACT_APP_BACKENDURL + '/register',
+            {
+                user: data.usuario,
+                email: data.email
+            },
+            {
+                withCredentials: true
+            })
+            .then(response => {
+                console.log(response);
+                setOpen(false);
+            })
     }
 
     return (
         <div className="Register">
+            {/* <Button>Show backdrop</Button> */}
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={open}
+                // onClick={handleClose}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
             <div className="container">
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <CustomTextField className='muiInput' required label="User" id="custom-css-outlined-input" {...register('usuario')}/>
-                    <CustomTextField className='muiInput' type="email" required label="E-mail" id="custom-css-outlined-input" {...register('email')}/>
+                    <CustomTextField className='muiInput' required label="User" id="custom-css-outlined-input" {...register('usuario')} />
+                    <CustomTextField className='muiInput' type="email" required label="E-mail" id="custom-css-outlined-input" {...register('email')} />
                     <div className="buttonsection">
                         <Button variant="outlined" color="success" onClick={login}> Log in </Button>
                         <Button variant="contained" color="success" type="submit"> Register </Button>
